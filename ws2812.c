dd96b34baa05ac0a65398633fe22e58889619df2
pycuber <pete4radio@icloud.com>
2025-03-28 08:08:24 -0700

    removed patterns and tested ok

--- ws2812.c
+++ ws2812.c
@@ -62,54 +62,6 @@ static inline uint32_t urgbw_u32(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
             (uint32_t) (b);
 }
 
-void pattern_snakes(PIO pio, uint sm, uint len, uint t) {
-    for (uint i = 0; i < len; ++i) {
-        uint x = (i + (t >> 1)) % 64;
-        if (x < 10)
-            put_pixel(pio, sm, urgb_u32(0xff, 0, 0));
-        else if (x >= 15 && x < 25)
-            put_pixel(pio, sm, urgb_u32(0, 0xff, 0));
-        else if (x >= 30 && x < 40)
-            put_pixel(pio, sm, urgb_u32(0, 0, 0xff));
-        else
-            put_pixel(pio, sm, 0);
-    }
-}
-
-void pattern_random(PIO pio, uint sm, uint len, uint t) {
-    if (t % 8)
-        return;
-    for (uint i = 0; i < len; ++i)
-        put_pixel(pio, sm, rand());
-}
-
-void pattern_sparkle(PIO pio, uint sm, uint len, uint t) {
-    if (t % 8)
-        return;
-    for (uint i = 0; i < len; ++i)
-        put_pixel(pio, sm, rand() % 16 ? 0 : 0xffffffff);
-}
-
-void pattern_greys(PIO pio, uint sm, uint len, uint t) {
-    uint max = 100; // let's not draw too much current!
-    t %= max;
-    for (uint i = 0; i < len; ++i) {
-        put_pixel(pio, sm, t * 0x10101);
-        if (++t >= max) t = 0;
-    }
-}
-
-typedef void (*pattern)(PIO pio, uint sm, uint len, uint t);
-const struct {
-    pattern pat;
-    const char *name;
-} pattern_table[] = {
-        {pattern_snakes,  "Snakes!"},
-        {pattern_random,  "Random data"},
-        {pattern_sparkle, "Sparkles"},
-        {pattern_greys,   "Greys"},
-};
-
 // TX
 void red(){
     for (int i = 0; i < NUM_PIXELS; ++i) {
@@ -163,17 +115,6 @@ int main() {
         sleep_ms(1000);
     }
 
-    while (1) {
-        int pat = rand() % count_of(pattern_table);
-        int dir = (rand() >> 30) & 1 ? 1 : -1;
-        puts(pattern_table[pat].name);
-        puts(dir == 1 ? "(forward)" : "(backward)");
-        for (int i = 0; i < 1000; ++i) {
-            pattern_table[pat].pat(pio, sm, NUM_PIXELS, t);
-            sleep_ms(10);
-            t += dir;
-        }
-    }
 
     // This will free resources and unload our program
     pio_remove_program_and_unclaim_sm(&ws2812_program, pio, sm, offset);
